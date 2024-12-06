1caf383f2e12bb9153b975588ba739d1c4f6d272
Wagner Cardoso <wagnerdev01@gmail.com>
2024-12-05 21:31:22 -0300

    refactor: last adjusments

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Função para pegar os dados dos inputs, mostrar um alerta na tela e limpar os inputs após o submit
 function sendContact() {
+  // Busca dos inputs
   const nameInput = document.getElementById('name-input');
   const emailInput = document.getElementById('email-input');
   const phoneInput = document.getElementById('phone-input');
   const messageInput = document.getElementById('message-input');
 
+  // Construção da mensagem
   const message = `Obrigado pelo contato, ${nameInput.value}!`;
-
   alert(message);
 
+  // Reset dos inputs
   nameInput.value = '';
   emailInput.value = '';
   phoneInput.value = '';
